34e5886dd2e2632b84ec6c093d7a625ca408d25e
raniellyferreira <ranifs1@hotmail.com>
2015-11-24 19:14:23 -0200

    Improvement

--- config/breadcrumbs.php
+++ config/breadcrumbs.php
@@ -4,22 +4,15 @@
 | BREADCRUMB CONFIG
 | -------------------------------------------------------------------
 | This file will contain some breadcrumbs' settings.
-|
-| $config['crumb_divider']		The string used to divide the crumbs
-| $config['tag_open'] 			The opening tag for breadcrumb's holder.
-| $config['tag_close'] 			The closing tag for breadcrumb's holder.
-| $config['crumb_open'] 		The opening tag for breadcrumb's holder.
-| $config['crumb_close'] 		The closing tag for breadcrumb's holder.
-|
 | Defaults provided for twitter bootstrap 2.0
 */
-
-$config['crumb_divider'] = '<span class="divider">/</span>';
-$config['tag_open'] = '<ul class="breadcrumb">';
-$config['tag_close'] = '</ul>';
-$config['crumb_open'] = '<li>';
-$config['crumb_last_open'] = '<li class="active">';
-$config['crumb_close'] = '</li>';
+$config['crumb']['divider'] 			= '<span class="divider">/</span>';
+$config['crumb']['full_tag_open'] 	= '<ul class="breadcrumb">';
+$config['crumb']['full_tag_close'] 	= '</ul>';
+$config['crumb']['tag_open'] 		= '<li>';
+$config['crumb']['tag_close'] 		= '</li>';
+$config['crumb']['last_tag_open'] 	= '<li class="active">';
+$config['crumb']['last_tag_close'] 	= '</li>';
 
 
 /* End of file breadcrumbs.php */
